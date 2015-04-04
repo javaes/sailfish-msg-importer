31074b62a4695df74ed40a79bb980bf2307a90b4
Jan <jan@vanesdonk.de>
2015-04-04 14:41:46 +0200

    removed deprecated methods, added some db methods

--- sail_sms.py
+++ sail_sms.py
@@ -34,7 +34,6 @@ class SMSParser(object):
         for sms in sms_list:
             if sms.attributes["protocol"].value == "0" or sms.attributes["protocol"].value == "193":
                 sailfish_sms_list.append(self._convert_to_sailfish(sms))
-        #self._group_by_remote_uid(sailfish_sms_list)
         logging.info("Converted %d SMS", len(sailfish_sms_list))
         return sailfish_sms_list
     
@@ -70,18 +69,16 @@ class SMSParser(object):
         entry["isAction"] = 0
         entry["hasExtraProperties"] = 0
         entry["hasMessageParts"] = 0
+        entry["messageToken"] = str(uuid.uuid4())
         if sms.attributes["type"].value == str(1):
             # received sms
             entry["direction"] = 1
-            entry["status"] = 0
-            entry["messageToken"] = str(uuid.uuid4())
+            entry["status"] = 0            
             entry["lastModified"] = "1970-01-01T01:00:00.000"
         elif sms.attributes["type"].value == str(2):
             # sent sms
             entry["direction"] = 2
             entry["status"] = 2
-            #TODO: messageToken
-            entry["messageToken"] = str(uuid.uuid4())
             mod_date = datetime.datetime.fromtimestamp(int(sms.attributes["date"].value) / 1000)          
             entry["lastModified"] = mod_date.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return entry
@@ -192,13 +189,6 @@ class SMSImporter(object):
                                             group_id))
         self.db.commit()
         logging.info("Imported SMS to Group %d", group_id)
-
-    def _group_exists(self, group_id):
-        query = "SELECT * FROM Groups WHERE id=%d" % group_id
-        self.c.execute(query)
-        if self.c.fetchone():
-            return True
-        return False
     
     def _add_new_group(self, remote_uid, type=0):
         query = "INSERT INTO Groups(localUid, remoteUids, type, lastModified)\
@@ -207,23 +197,29 @@ class SMSImporter(object):
         self.db.commit()
         
     def _get_group(self, remote_uid):
-        query = "SELECT id FROM Groups WHERE remoteUids = '%s'" % remote_uid
+        query = "SELECT id FROM Groups WHERE remoteUids = '%s';" % remote_uid
         self.c.execute(query)
         result = self.c.fetchone()
         if result is not None:
             return result[0]
         return None
-        
-    def _generate_insert_query(self, sf_sms):
-        field_names = ""
-        values = ""
-        for k, v in sf_sms.items():
-            field_names += str(k) + ", "
-            if type(v) is str and v != "null":
-                v = "\"" + v + "\"" 
-            values += str(v) + ", "
-        field_names = field_names[:-2]
-        values = values[:-2]
-        query = "INSERT INTO Events(%s) VALUES(%s)" % (field_names, values)
-        return query
-            
+    
+    def get_group_count(self):
+        query = "SELECT * FROM Groups;"
+        self.c.execute(query)
+        return len(self.c.fetchall())
+    
+    def get_msg_count(self):
+        query = "SELECT * FROM Events;"
+        self.c.execute(query)
+        return len(self.c.fetchall())
+    
+    def remove_all_groups_and_msgs(self):
+        """
+        Removes all messages and their groups from the database
+        """
+        query = "DELETE FROM Events WHERE 1 = 1;"
+        self.c.execute(query)
+        query = "DELETE FROM Groups WHERE 1 = 1;"
+        self.c.execute(query)
+        self.db.commit()
